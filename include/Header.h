bcd28fdd14f427840fa48993ae5dd08f43347e42
jiawang <jiawang.bian@gmail.com>
2017-04-09 22:45:34 +0800

    4.9

---
+++ include/Header.h
@@ -0,0 +1,20 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include <opencv2/opencv.hpp>
+#include <vector>
+#include <iostream>
+#include <ctime>
+#include <map>
+using namespace std;
+using namespace cv;
+
+#ifdef USE_GPU
+	#include <opencv2/cudafeatures2d.hpp>
+	using cuda::GpuMat;
+#endif
+
